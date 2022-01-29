f94ec8787b49dfe122780d1ea675c68b3ab3094e
betocostadev <robertomarx@gmail.com>
2022-01-29 14:33:52 -0300

    added Dashboard and Tweet components

--- src/store/actions/shared.js
+++ src/store/actions/shared.js
@@ -2,15 +2,18 @@ import { getInitialData } from '../../utils/api'
 import { receiveUsers } from './users'
 import { receiveTweets } from './tweets'
 import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 // Using a fake logged user
 const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
   return async (dispatch) => {
+    dispatch(showLoading())
     const { users, tweets } = await getInitialData()
     dispatch(receiveUsers(users))
     dispatch(receiveTweets(tweets))
     dispatch(setAuthedUser(AUTHED_ID))
+    dispatch(hideLoading())
   }
 }
